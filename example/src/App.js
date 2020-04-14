119a40316fcab5767296b864d2738b39b5268e10
RehmatFalcon <niroulakushal31@gmail.com>
2020-04-14 20:28:34 +0545

    Feat: Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,55 @@
-import React from 'react'
-import { useMyHook } from '@rehmat-falcon/use-local-storage'
+import React, { useEffect } from 'react'
+import { useLocalStorage } from '@rehmat-falcon/use-local-storage';
+import "./index.css";
 
 const App = () => {
-  const example = useMyHook()
+  let localStorageManager = {};
+  localStorageManager = useLocalStorage("name", "Initial Name");
+  const firstInputRef = React.createRef();
+  const updateFirst = () => {
+    localStorageManager.update(firstInputRef.current.value);
+  };
+  const removeStorage = () => {
+    localStorageManager.remove();
+    firstInputRef.current.value = "";
+  };
+  const {value} = localStorageManager.state;
   return (
-    <div>
-      {example}
+    <div className="row">
+      <div className="col card">
+        <div className="card-header">
+          <h3 className="title">
+            Update Value
+          </h3>
+        </div>
+        <div className="card-body">
+          <input type="text" className="input full" ref={firstInputRef} defaultValue={localStorageManager.state.value} 
+            placeholder="Value to save in localstorage"
+          />
+          <button className="btn btn-primary" onClick={updateFirst}>
+            Update
+          </button>
+          <button className="btn btn-danger" onClick={removeStorage}>
+            Delete
+          </button>
+        </div>
+      </div>
+      <div className="col card">
+        <div className="card-header">
+          <h3 className="title">
+            Local storage values
+          </h3>
+        </div>
+        <div className="card-body">
+          Key : {localStorageManager.key} <br/>
+          Exists : {localStorageManager.state.exists && <span>Yes</span>  }
+                   {!localStorageManager.state.exists && <span>No</span>}
+                   <br/>
+          Value : {localStorageManager.state.value} <br/>
+          Initial Value : {localStorageManager.nonReactiveState.value}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import * as React from 'react'
+import {useObjectState} from '@rehmat-falcon/use-object-state';
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
-
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
-    }
-  }, [])
-
-  return counter
-}
+export const useLocalStorage = (key, initial = "", override = false) => {
+  let initialState;
+  if(override) {
+    window.localStorage.setItem(key,initial);
+    initialState = {
+      value : initial,
+      exists : true
+    };
+  } 
+  else {
+    const item = window.localStorage.getItem(key);
+    initialState = {
+      value : item,
+      exists : item !== null
+    };
+  }
+  const [state, updateState] = useObjectState(initialState);
+  const update = (newValue) => {
+    window.localStorage.setItem(key, newValue);
+    updateState({
+      value : newValue,
+      exists : true
+    });
+  };
+  const remove = () => {
+    window.localStorage.removeItem(key);
+    updateState({
+      value : null,
+      exists : false
+    });
+  };
+  const [nonReactiveState] = useObjectState(initialState);
+  return {
+    state,
+    nonReactiveState,
+    key,
+    update,
+    remove,
+  };
+};
