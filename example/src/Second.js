4da555344e1305a39e604cc1cf0eabd502faf556
RehmatFalcon <niroulakushal31@gmail.com>
2020-04-15 08:54:05 +0545

    Feat: show json viewer

--- example/src/Second.js
+++ example/src/Second.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import {useLocalStorage} from '@rehmat-falcon/use-local-storage';
+import React, { useCallback } from 'react';
+import { useLocalStorage } from '@rehmat-falcon/use-local-storage';
 import './index.css';
+import ReactJson from 'react-json-view';
 
 const Second = () => {
-    const localStorageManager = useLocalStorage("obj_value", {
-        a : 123,
-        b : 456,
-        ef : "Core"
-    },true);
-    console.log(localStorageManager.state);
+    const localStorageManager = useLocalStorage("obj_value", {});
+    const handleChange = useCallback(e => {
+        localStorageManager.update(e.updated_src);
+    });
     return (
         <div className="row">
-            <div className="col card">
+            <div className="col card" style={{ width: '50%' }}>
                 <div className="card-header">
                     <h3 className="card-title">
                         Value
                     </h3>
                 </div>
                 <div className="card-body">
-                    View in dev tools
+                    <ReactJson src={localStorageManager.state.value} theme="mocha" iconStyle="square" enableClipboard={true} onEdit={handleChange} onAdd={handleChange} onDelete={handleChange} />
                 </div>
             </div>
-        </div>        
+            <div className="col card" style={{ width: '50%' }}>
+                <div className="card-header">
+                    <h3 className="card-title">
+                        Value
+                    </h3>
+                </div>
+                <div className="card-body">
+                    <ReactJson src={localStorageManager.state.value} theme="mocha" iconStyle="square" enableClipboard={true} onEdit={handleChange} onAdd={handleChange} onDelete={handleChange} />
+                </div>
+            </div>
+        </div>
     );
 };
 export default Second;
